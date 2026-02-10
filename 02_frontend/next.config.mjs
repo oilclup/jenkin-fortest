74394396bd373cb705b16cd0f26352dbc27a2435
oilclup <58043754+oilclup@users.noreply.github.com>
2026-02-10 17:45:50 +0700

    updockerfie

--- 02_frontend/next.config.mjs
+++ 02_frontend/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const API_HOST = process.env.API_HOST || 'http://localhost:3001';
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3001';
 
 const nextConfig = {
   output: 'standalone',
